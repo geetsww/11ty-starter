be312a5ca6ffce7733a3f6b4c3554e5ed8052c05
Benjamin Geets <bge@tuta.io>
2023-03-08 10:44:10 +0100

    init

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+module.exports = (eleventyConfig) =>{
+  eleventyConfig.addPlugin(eleventySass);
+
+  eleventyConfig.addPassthroughCopy({"public":"/"});
+  return{
+    dir:{
+      input:'src'
+    }
+  }
+}
